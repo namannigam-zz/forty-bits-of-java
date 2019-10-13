2f3358d2705bbcd1b2ac348daeaf28028f69e2f8
Naman Nigam <namannigam12@gmail.com>
2019-10-13 20:31:20 +0530

    Java stream series completed.

--- src/main/java/edu/forty/bits/__Trial__.java
+++ src/main/java/edu/forty/bits/__Trial__.java
@@ -1,8 +1,24 @@
 package edu.forty.bits;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class __Trial__ {
-    public static void main(List<Byte> listByte) {
+
+    public static void main(String... ar) {
+        List<String> toRemove = Arrays.asList("\u201C", "\u201D");
+        List<String> toRemove1 = Arrays.asList("\u2018", "\u2019");
+
+        String text = "KURT’X45T”YUZXC";
+        for (String toRem : toRemove) {
+            text = text.replaceAll(toRem, "\"");
+        }
+        System.out.println("---text--- " + text);
+
+        for (String toRem : toRemove1) {
+            text = text.replaceAll(toRem, "'");
+        }
+
+        System.out.println("---text--- " + text);
     }
 }
---
+++ src/main/java/edu/forty/bits/functional/ParallelStreamPerformance.java
@@ -0,0 +1,20 @@
+package edu.forty.bits.functional;
+
+public class ParallelStreamPerformance {
+    /*
+     * The previous installment identified several factors that might cause a parallel execution to lose efficiency:
+     *
+     * The source is expensive to split, or splits unevenly.
+     * Merging partial results is expensive.
+     * The problem doesn’t admit sufficient exploitable parallelism.
+     * The layout of the data results in poor access locality.
+     * There’s not enough data to overcome the startup costs of parallelism.
+     */
+
+
+    /*
+     * Stream pipelines whose merge steps are O(n)— such as those using the sorted()
+     * or collect(Collectors.joining()) terminal operations
+     * — might see their parallelism limited by this effect.
+     */
+}
