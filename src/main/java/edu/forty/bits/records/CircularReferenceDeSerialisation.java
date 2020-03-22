eeabe4321a2bab7ad4da8bb82933f388c6dd9f42
Naman Nigam <namannigam12@gmail.com>
2020-03-22 13:45:13 +0530

    more around the records migration and deserialisation

---
+++ src/main/java/edu/forty/bits/records/CircularReferenceDeSerialisation.java
@@ -0,0 +1,35 @@
+package edu.forty.bits.records;
+
+import lombok.Builder;
+
+import java.io.*;
+
+public class CircularReferenceDeSerialisation {
+
+    @Builder
+    private record One(String one, Two two) implements Serializable {
+        public One(String one) {
+            this(one, null);
+        }
+    }
+
+    private record Two(Integer two, Three three) implements Serializable {
+    }
+
+    private record Three(Long three, One one) implements Serializable {
+    }
+
+    public static void main(String... args) throws IOException, ClassNotFoundException {
+        One one = new One("tail");
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("records.bin"))) {
+            out.writeObject(new One("one", new Two(2, new Three(3L, one))));
+            out.writeObject(null);
+        }
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("records.bin"))) {
+            One allInOne;
+            while ((allInOne = (One) in.readObject()) != null) {
+                System.out.println(allInOne);
+            }
+        }
+    }
+}
---
+++ src/main/java/edu/forty/bits/records/equals/CityRecord.java
@@ -0,0 +1,4 @@
+package edu.forty.bits.records.equals;
+
+record CityRecord(Integer id, String name) {
+}
