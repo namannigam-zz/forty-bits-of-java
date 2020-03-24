869c15e423741635733726a47920781de627281d
Naman Nigam <namannigam12@gmail.com>
2020-03-24 07:26:11 +0530

    cyclicl more

--- src/main/java/edu/forty/bits/records/CircularReferenceDeSerialisation.java
+++ src/main/java/edu/forty/bits/records/CircularReferenceDeSerialisation.java
@@ -6,6 +6,12 @@ import java.io.*;
 
 public class CircularReferenceDeSerialisation {
 
+    private record Cyclic(Cyclic cycle) implements Serializable {
+        public Cyclic() {
+            this(new Cyclic(null));
+        }
+    }
+
     @Builder
     private record One(String one, Two two) implements Serializable {
         public One(String one) {
@@ -23,12 +29,18 @@ public class CircularReferenceDeSerialisation {
         One one = new One("tail");
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("records.bin"))) {
             out.writeObject(new One("one", new Two(2, new Three(3L, one))));
+            out.writeObject(new Cyclic(new Cyclic(new Cyclic(new Cyclic()))));
             out.writeObject(null);
         }
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("records.bin"))) {
             One allInOne;
             while ((allInOne = (One) in.readObject()) != null) {
                 System.out.println(allInOne);
+            }
+
+            Cyclic cyclic;
+            while ((cyclic = (Cyclic) in.readObject()) != null) {
+                System.out.println(cyclic);
             }
         }
     }
