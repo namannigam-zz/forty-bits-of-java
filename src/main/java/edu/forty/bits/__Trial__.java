6e6c7f2c01b4c873c0e6b205ede15f081981c3d0
Naman Nigam <namannigam12@gmail.com>
2020-08-04 23:21:30 +0530

    `date`

--- src/main/java/edu/forty/bits/__Trial__.java
+++ src/main/java/edu/forty/bits/__Trial__.java
@@ -1,9 +1,10 @@
 package edu.forty.bits;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.*;
-import java.util.function.*;
-import java.util.stream.*;
+import java.util.stream.Stream;
 
 public class __Trial__ {
 
@@ -27,6 +28,9 @@ public class __Trial__ {
     }
 
     private static void solve() {
-
+        Collection<String> c = List.of("1", "2", "3", "4", "5");
+        String result = c.stream().collect(() -> "",
+                (s, str) -> s.concat(str), (s1, str1) -> s1.concat(str1));
+        System.out.print(result);
     }
 }
