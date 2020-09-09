2d2ead67991b4001110825c8ed8bd42e3c646b8e
Naman Nigam <namannigam12@gmail.com>
2020-09-09 22:35:37 +0530

    Optioinal if presnent or elsee
    Signed-off-by: Naman Nigam <namannigam12@gmail.com>

--- src/main/java/edu/forty/bits/utilities/OptionalOrElse.java
+++ src/main/java/edu/forty/bits/utilities/OptionalOrElse.java
@@ -5,26 +5,18 @@ import java.util.Optional;
 
 public class OptionalOrElse {
 
-    // return true: if present and number of lines != 0
     boolean isValid(File optFile) {
-        return Optional.ofNullable(optFile).map(this::isZeroLine).orElse(false);
-    }
-
-    boolean isValid(Optional<File> optFile) {
-        optFile.ifPresentOrElse(this::doWork, this::doNothing);
-        return optFile.filter(this::isZeroLine).isPresent();
+        Optional.ofNullable(optFile).ifPresentOrElse(this::doWork, this::doNothing);
+        return Optional.ofNullable(optFile).filter(this::isZeroLine).isPresent();
     }
 
     private boolean isZeroLine(File f) {
         return f.canRead();
     }
 
-    private void doWork(File f){
-
+    private void doWork(File f) {
     }
 
     private void doNothing() {
-
     }
-
 }
