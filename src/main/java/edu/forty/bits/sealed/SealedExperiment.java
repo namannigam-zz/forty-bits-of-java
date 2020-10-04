7ef786205520806a92a539e9e5661248f3f376e2
Naman Nigam <namannigam12@gmail.com>
2020-10-04 11:47:22 +0530

    java 15 and sealed classes

---
+++ src/main/java/edu/forty/bits/sealed/SealedExperiment.java
@@ -0,0 +1,36 @@
+package edu.forty.bits.sealed;
+
+public class SealedExperiment {
+
+    public static void main(String[] args) {
+        Integral integral = new Integral();
+        integral.bar();
+        integral.foo();
+    }
+
+    public sealed interface Int permits Some {
+        void foo();
+
+        void bar();
+    }
+
+    static abstract sealed class Some implements Int permits Integral {
+        public void foo() {
+            System.out.println("sealed some");
+        }
+    }
+
+    static non-sealed class Integral extends Some {
+
+        @Override
+        public void bar() {
+            System.out.println("bar from non-sealed class");
+        }
+
+        @Override
+        public void foo() {
+            System.out.println("foo from non-sealed class");
+        }
+
+    }
+}
